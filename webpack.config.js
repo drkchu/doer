6c8caa51d7789c751f7a4bb354692e58c1068d65
drkchu <derekdbchu@gmail.com>
2024-01-03 18:54:09 -0800

    Set up project, add favicon

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: "DOER",
+    favicon: "./src/assets/img/memo.png"
+  })],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
